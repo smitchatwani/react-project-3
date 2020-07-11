4ca2d831acd9ece53bfe44d92f0d48d9c9a1c27e
Smit Chatwni <smitchatwani113@gmail.com>
2020-07-12 01:55:23 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+app.use(express.json({ extended: false })); //access body data
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome' }));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
